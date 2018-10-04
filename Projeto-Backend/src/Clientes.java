c5fb1a1cc749e3d0bf28691362694531092e17b7
Glória Maria Dias <gloria.dias@zup.com.br>
2018-10-04 20:40:00 -0300

    Add files via upload

---
+++ Projeto-Backend/src/Clientes.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+public class Clientes {
+
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private Logradouro endereço;
+    private ArrayList<Produto> comprados = new ArrayList<Produto>();
+    private ArrayList<Produto> carrinho = new ArrayList<Produto>();
+
+    public Clientes(String nome, String telefone, String cpf, Logradouro endereço)
+    {
+        this.nome=nome;
+        this.telefone=telefone;
+        this.cpf=cpf;
+        this.endereço=endereço;
+    }
+
+    public String Comprar()
+    {
+         StringBuilder compra= new StringBuilder();
+        for (Produto aCarrinho : carrinho) {
+            comprados.add(aCarrinho);
+            compra.append("Produto: ").append(aCarrinho.getNome()).append(" Valor: ").append(aCarrinho.getPreço()).append("::: ");
+        }
+       carrinho.clear();
+        return compra.toString();
+    }
+
+    public void AdicionarAoCarrinho(Produto produto)
+    {
+       carrinho.add(produto);
+    }
+
+    public void RemoverDoCarrinho(Produto produto)
+    {
+       for (int i=0; i<carrinho.size(); i++){
+           if (carrinho.get(i).equals(produto)){
+               carrinho.remove(produto);
+           }
+       }
+    }
+
+    public ArrayList<Produto> getCarrinho()
+    {
+      return carrinho;
+    }
+
+    public void setCarrinho(ArrayList<Produto> carrinho)
+    {
+        this.carrinho = carrinho;
+    }
+
+    public void setComprados(ArrayList<Produto> comprados)
+    {
+        this.comprados = comprados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCPF() {
+        return cpf;
+    }
+
+    public Logradouro getEndereço() {
+        return endereço;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereço(Logradouro endereço) {
+        this.endereço = endereço;
+    }
+}
---
+++ Projeto-Backend/src/Loja.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Loja
+{
+    private String nome="";
+    private String cnpj="989.556.567/0001-35";
+    private Logradouro endereco=new Logradouro("Emidia Saraiva", "Carajas", "Minas Gerais", "Uberlândia", "38408587", 77);
+
+    private ArrayList<Produto> vendidos = new ArrayList<Produto>();
+    private ArrayList<Produto> estoque = new ArrayList<Produto>();
+
+
+    public void AdicionaProdutoEstoque(Produto produto)
+
+    {
+        estoque.add(produto);
+    }
+
+    public void VendeProduto(Produto produto, int quantidade)
+    {
+        vendidos.add(produto);
+        for(int i=0; i<=estoque.size(); i++)
+        {
+           if(estoque.get(i).equals(produto))
+           {
+               produto.setQuantidade(produto.getQuantidade()-quantidade);
+           }
+        }
+    }
+
+    public void AdicionaEmEstoque(Produto produto, int quantidade)
+    {
+        for(int i=0; i<=estoque.size(); i++)
+        {
+            if(estoque.get(i).equals(produto))
+            {
+                produto.setQuantidade(produto.getQuantidade()+quantidade);
+            }
+        }
+    }
+
+
+}
