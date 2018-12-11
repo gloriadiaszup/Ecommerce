62fb5435dda5ce2a4f0d5ed683e99d786ea3594d
Gloria <gloria.dias@zup.com.br>
2018-12-11 17:54:11 -0200

    correção dos scripts para o banco de dados

--- InsertProdutos.sql
+++ InsertProdutos.sql
@@ -1,3 +1,6 @@
+USE [E-commerce]
+GO
+
 INSERT INTO [dbo].[Tabela_Produtos]
            ([NOME]
            ,[TAMANHO]
@@ -5,11 +8,27 @@ INSERT INTO [dbo].[Tabela_Produtos]
            ,[PREÇO]
            ,[CÓDIGO DO PRODUTO])
      VALUES
-           (NOME,
-           TAMANHO,
-           QUANTIDADE,
-           PREÇO,
-           CÓDIGO DO PRODUTO)
+           ('REGATA COM BOTÕES EM LINHO', 
+		   'P',
+		   60,
+		   99.90,
+		   546236979)
 GO
+
+INSERT INTO [dbo].[Tabela_Produtos]
+           ([NOME]
+           ,[TAMANHO]
+           ,[QUANTIDADE]
+           ,[PREÇO]
+           ,[CÓDIGO DO PRODUTO])
+     VALUES
+           ('SAIA MIDI CLOCHARD COM BOTÕES EM LINHO', 
+		   'P',
+		   60,
+		   159.90,
+		   54635233)
+GO
+
+
 
 
